112e77f11503353b883909b667b328b48dbd3028
Gshivamm <2021wa86276@wilp.bits-pilani.ac.in>
2025-07-13 23:41:33 +0530

    Modified App.java to test CI trigger

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from CI-Demo-Project!");
+	System.out.println("Hello again from Jenkins CI!");
     }
 }
